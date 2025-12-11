3e283078a48b7334a1281b67c405c85372fd0528
Ingo Wolf <me@ingowolf.au>
2025-12-11 14:23:45 +1100

    add links

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,9 +1,19 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { FaSlack } from "react-icons/fa";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: "Hack Club Mobile",
     },
+    githubUrl: "https://github.com/TheAwesomeAJ/Hackclub-Mobile",
+    links: [
+      {
+        type: "icon",
+        icon: <FaSlack />,
+        text: "Slack",
+        url: "https://hackclub.enterprise.slack.com/archives/C09NTG60T8X",
+      },
+    ],
   };
 }
